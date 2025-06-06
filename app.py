dcbfb0dd3e66481d3d46d67c05edb05576cbc3ea
mahadnav <mahadn20@gmail.com>
2025-06-06 15:10:00 +0500

    ''

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ Draw(export=True, draw_options={'rectangle': True, 'polygon': False, 'circle': F
 st_map = st_folium(m, width=700, height=500, returned_objects=["last_active_drawing"])
 
 def download_worldpop(country_code="PAK", year="2020"):
-    base_url = f"https://data.worldpop.org/GIS/Population/Global_2000_2020/{year}/{country_code}/{(country_code).lower()}_ppp_{year}_UNadj_constrained.tif"
+    base_url = f"https://data.worldpop.org/GIS/Population/Global_2000_2020/{year}/{country_code}/{country_code.lower()}_ppp_2020_UNadj.tif"
     st.write(f"Downloading WorldPop data from: {base_url}")
     response = requests.get(base_url, stream=True)
 
