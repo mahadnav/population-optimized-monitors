30d6ad6a169bf5536ac23e8149eab0e3d801f40f
mahadnav <mahadn20@gmail.com>
2025-06-08 18:30:26 +0500

    ''

--- app.py
+++ app.py
@@ -144,7 +144,7 @@ st.markdown("#### Define Your Airshed")
 m = folium.Map(zoom_start=8, tiles="CartoDB positron")
 from folium.plugins import Draw
 Draw(export=False, draw_options={'rectangle': True, 'polygon': False, 'circle': False, 'circlemarker': False, 'marker': False, 'polyline': False}).add_to(m)
-st_map = st_folium(m, width=1700, height=1000, returned_objects=["last_active_drawing"])
+st_map = st_folium(m, width=1700, height=1000, use_container_width=True, returned_objects=["last_active_drawing"])
 
 # --- Logic to detect a new drawing and require confirmation ---
 if st_map and st_map.get("last_active_drawing"):
