ebe678102628b2676a4ac4419f4e9588fba089c5
mahadnav <mahadn20@gmail.com>
2025-06-12 19:05:11 +0500

    ''

--- app.py
+++ app.py
@@ -505,13 +505,13 @@ if st.session_state.airshed_confirmed:
                     popup=f"Proposed Monitor #{index+1}<br>Lat: {row['lat']:.4f}, Lon: {row['lon']:.4f}"
                 ).add_to(proposed_fg)
             proposed_fg.add_to(m_final)
-            # add_tile_layers(m_final)
+            add_tile_layers(m_final)
 
-            deployed = pd.read_csv("deployed_monitors.csv") if os.path.exists("deployed_monitors.csv") else None
-            if deployed is not None and not deployed.empty:
-                for index, row in deployed.iterrows():
-                    folium.Marker(location=[row['latitude'], row['longitude']], icon=folium.Icon(color='green')).add_to(m_final)
-            monitor_map = st_folium(m_final, use_container_width=True, height=1050)
+            # deployed = pd.read_csv("deployed_monitors.csv") if os.path.exists("deployed_monitors.csv") else None
+            # if deployed is not None and not deployed.empty:
+            #     for index, row in deployed.iterrows():
+            #         folium.Marker(location=[row['latitude'], row['longitude']], icon=folium.Icon(color='green')).add_to(m_final)
+            monitor_map = st_folium(m_final, use_container_width=True, height=1000)
         with tab2:
             st.dataframe(final_df.style.format({'lat': '{:.5f}', 'lon': '{:.5f}'}))
             final_csv = final_df.to_csv(index=False).encode('utf-8')
